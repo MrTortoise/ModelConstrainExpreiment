3cc10e9770b0b8095150fa90049162d0ffb37178
john <themrtortoise@gmail.com>
2015-07-30 19:17:08 +0100

    defined card - red time to generate tests

--- HandAnalyser/HandAnalyser.Test/CardModel.cs
+++ HandAnalyser/HandAnalyser.Test/CardModel.cs
@@ -11,20 +11,39 @@ namespace HandAnalyser.Test
     /// <summary>
     /// This models the values of a card
     /// </summary>
-    [TestFixture]
     class CardModel
     {
-        [Test]
+       
         public void TestValidCardRanges(int value, int suit)
         {
-            Contract.Requires(value >= 1 && value <= 13);
-            Contract.Requires(suit >= 1 && suit <= 4);
-
             var card = new Card(value, suit);
 
             Assert.AreEqual(value, card.Value);
             Assert.AreEqual(suit, card.Suit);
+        }
+    }
 
+    public class Card
+    {
+        private  int _value;
+        private  int _suit;
+
+        public Card(int value, int suit)
+        {
+            _value = value;
+            _suit = suit;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the card between 1 and 12
+        /// </summary>
+        public int Value { get { return _value; }
+            set { _value = value; }
+        }
+
+        public int Suit  {
+            get { return _suit;}
+            set { _suit = value; }
         }
     }
 }
