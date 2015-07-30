60ed5db66377f5a533e8facd4cd8dd98ca79bab1
john <themrtortoise@gmail.com>
2015-07-30 19:06:56 +0100

    Defining card - red

--- HandAnalyser/HandAnalyser.Test/CardModel.cs
+++ HandAnalyser/HandAnalyser.Test/CardModel.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace HandAnalyser.Test
 {
     /// <summary>
     /// This models the values of a card
     /// </summary>
+    [TestFixture]
     class CardModel
     {
+        [Test]
+        public void TestValidCardRanges(int value, int suit)
+        {
+            Contract.Requires(value >= 1 && value <= 13);
+            Contract.Requires(suit >= 1 && suit <= 4);
 
+            var card = new Card(value, suit);
+
+            Assert.AreEqual(value, card.Value);
+            Assert.AreEqual(suit, card.Suit);
+
+        }
     }
 }
