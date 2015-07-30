2780ad427da4578f97202a5218595adc511241fe
john <themrtortoise@gmail.com>
2015-07-30 19:40:18 +0100

    installed https://visualstudiogallery.msdn.microsoft.com/bd30bf3f-4183-4b00-a245-1875316b8cd3
    also some butchery to move card into the correct place

---
+++ HandAnalyser/HandAnalyser/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace HandAnalyser
+{
+    public class Card
+    {
+        private  int _value;
+        private  int _suit;
+
+        public Card(int value, int suit)
+        {
+            _value = value;
+            _suit = suit;
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the card between 1 and 12
+        /// </summary>
+        public int Value { get { return _value; }
+            set { _value = value; }
+        }
+
+        public int Suit  {
+            get { return _suit;}
+            set { _suit = value; }
+        }
+    }
+}
